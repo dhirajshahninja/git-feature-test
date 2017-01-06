11c6bd8a3541f43e5d7894125da1647771ea5173
dhirajshahninja <dhirajshahninja@gmail.com>
2017-01-06 12:33:56 +0530

    comit from master for test the feature

--- testfeature.js
+++ testfeature.js
@@ -3,6 +3,6 @@ console.log('changes by featuresnow chnages by master in fist bline');
 
 console.log('Thanks sir');
 
-console.log('this is chanegs from master');
+console.log('this is chanegs from master'); 
 console.log('changes by featuresnow chnages by master');
 console.log('now second line changes by master');
