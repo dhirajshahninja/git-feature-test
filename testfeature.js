4d00d11bd27ec075e9ae4688d14fe80762ec9d06
dhirajshahninja <dhirajshahninja@gmail.com>
2017-01-06 12:48:20 +0530

    now commit from features for testing conflict accoures in master or not

--- testfeature.js
+++ testfeature.js
@@ -1,8 +1,12 @@
 /* this file for test branch update or not by file */
 console.log('changes by featuresnow chnages by master in fist bline');
 
+console.log('no conflict accures in jacva');
+
 console.log('Thanks sir');
 
+console.log('here i want to check in master conflict accures or not');
 console.log('this is chanegs from master');
+
 console.log('changes by featuresnow chnages by master');
 console.log('now second line changes by master');
