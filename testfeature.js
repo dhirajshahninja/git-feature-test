c077617856565263ddd50742172948d1c7ea7ef6
dhirajshahninja <dhirajshahninja@gmail.com>
2017-01-06 11:58:29 +0530

    resolve

--- testfeature.js
+++ testfeature.js
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
 /* this file for test branch update or not by file */
 
 console.log('Thanks sir');
+=======
+console.log('this is chanegs from master');
+console.log('now second line changes by master');
+>>>>>>> Stashed changes
