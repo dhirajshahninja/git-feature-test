6fba66e070f8ea1bdce654aac535ac5cc3bbac08
dhirajshahninja <dhirajshahninja@gmail.com>
2017-01-06 12:21:26 +0530

    now changes from master for check cofilct

--- testfeature.js
+++ testfeature.js
@@ -1,7 +1,8 @@
 /* this file for test branch update or not by file */
+console.log('changes by featuresnow chnages by master in fist bline');
 
 console.log('Thanks sir');
 
 console.log('this is chanegs from master');
-console.log('changes by features');
+console.log('changes by featuresnow chnages by master');
 console.log('now second line changes by master');
